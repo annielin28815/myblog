9739f755d725213dcae6bccf62664292e3b86536
Annie <annielin28815@gmail.com>
2021-02-02 20:32:15 +0800

    add jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,20 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import "jquery";
+import $ from "jquery";
+window.jQuery = $
+window.$ = $;
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20) 
+      $(".navbar").addClass("sticky");
+    else
+      $(".navbar").removeClass("sticky");
+  });
+
+  $('.menu-toggler').click(function(){
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+});
